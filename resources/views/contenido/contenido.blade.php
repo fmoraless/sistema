744050121844b62c1e51087ddb15ce25f94abfa8
Francisco <Francisco@fmorales>
2018-06-05 22:06:05 -0400

    Instalación Laravel 5.6, hasta modelo.

---
+++ resources/views/contenido/contenido.blade.php
@@ -0,0 +1,45 @@
+@extends('principal')
+@section('contenido')
+<template v-if="menu==0">
+    <div class="div">
+    <example-component></example-component>
+    </div>
+</template>
+<template v-if="menu==1">
+    <h1>Contenido menu 1</h1>
+</template>
+<template v-if="menu==2">
+    <h1>Contenido menu 2</h1>
+</template>
+<template v-if="menu==3">
+    <h1>Contenido menu 3</h1>
+</template>
+<template v-if="menu==4">
+    <h1>Contenido menu 4</h1>
+</template>
+<template v-if="menu==5">
+    <h1>Contenido menu 5</h1>
+</template>
+<template v-if="menu==6">
+    <h1>Contenido menu 6</h1>
+</template>
+<template v-if="menu==7">
+    <h1>Contenido menu 7</h1>
+</template>
+<template v-if="menu==8">
+    <h1>Contenido menu 8</h1>
+</template>
+<template v-if="menu==9">
+    <h1>Contenido menu 9</h1>
+</template>
+<template v-if="menu==10">
+    <h1>Contenido menu 10</h1>
+</template>
+<template v-if="menu==11">
+    <h1>Contenido menu 11</h1>
+</template>
+<template v-if="menu==12">
+    <h1>Contenido menu 12</h1>
+</template>
+
+       @endsection
