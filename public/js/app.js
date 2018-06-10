f10eb0485361741ddfb7543a1dddbb4279fe1432
Francisco <Francisco@fmorales>
2018-06-09 23:45:03 -0400

    Insertar categorias

--- public/js/app.js
+++ public/js/app.js
@@ -47875,7 +47875,19 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 console.log(error);
             });
         },
-        registrarCategoria: function registrarCategoria() {},
+        registrarCategoria: function registrarCategoria() {
+            var me = this;
+
+            axios.post('/categoria/registrar', {
+                'nombre': this.nombre,
+                'descripcion': this.descripcion
+            }).then(function (response) {
+                me.cerrarModal();
+                me.listarCategoria();
+            }).catch(function (error) {
+                console.log(error);
+            });
+        },
         cerrarModal: function cerrarModal() {
             this.modal = 0;
             this.tituloModal = '';
@@ -48133,7 +48145,7 @@ var render = function() {
                             }
                           ],
                           staticClass: "form-control",
-                          attrs: { type: "email", placeholder: "Enter Email" },
+                          attrs: { type: "email", placeholder: "Descripción" },
                           domProps: { value: _vm.descripcion },
                           on: {
                             input: function($event) {
@@ -48170,7 +48182,12 @@ var render = function() {
                       "button",
                       {
                         staticClass: "btn btn-primary",
-                        attrs: { type: "button" }
+                        attrs: { type: "button" },
+                        on: {
+                          click: function($event) {
+                            _vm.registrarCategoria()
+                          }
+                        }
                       },
                       [_vm._v("Guardar")]
                     )
--- resources/assets/js/components/Categoria.vue
+++ resources/assets/js/components/Categoria.vue
@@ -109,14 +109,14 @@
                                 <div class="form-group row">
                                     <label class="col-md-3 form-control-label" for="email-input">Descripción</label>
                                     <div class="col-md-9">
-                                        <input type="email" v-model="descripcion" class="form-control" placeholder="Enter Email">
+                                        <input type="email" v-model="descripcion" class="form-control" placeholder="Descripción">
                                     </div>
                                 </div>
                             </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" @click="cerrarModal()">Cerrar</button>
-                            <button type="button" v-if="tipoAccion==1" class="btn btn-primary">Guardar</button>
+                            <button type="button" v-if="tipoAccion==1" class="btn btn-primary" @click="registrarCategoria()">Guardar</button>
                             <button type="button" v-if="tipoAccion==2" class="btn btn-primary">Actualizar</button>
                         </div>
                     </div>
@@ -174,7 +174,17 @@
                 });
             },
             registrarCategoria(){
-                
+                let me = this;
+
+                axios.post('/categoria/registrar',{
+                    'nombre': this.nombre,
+                    'descripcion': this.descripcion
+                }).then(function (response) {
+                    me.cerrarModal();
+                    me.listarCategoria();
+                }).catch(function(error) {
+                    console.log(error);
+                });
             },
             cerrarModal(){
                 this.modal=0;
