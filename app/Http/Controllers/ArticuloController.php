e256964df9f449be0d20600c1a8f35ecd58ff012
Francisco <Francisco@fmorales>
2019-01-01 14:26:38 -0300

    Controlador Artículos.

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Articulo;
+
+class ArticuloController extends Controller {
+
+    public function index(Request $request) {
+        //$categorias = DB::table('categorias')->paginate(5);
+        if (!$request->ajax()) return redirect('/');
+
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+
+        if ($buscar == '') {
+            $articulos = Articulo::join('categorias', 'articulos.idcategoria', '=', 'categorias.id')
+                            ->select('articulos.id', 'articulos.idcategoria', 'articulos.codigo', 'articulos.nombre', 'categorias.nombre as nombre_categoria', 'articulos.precio_venta', 'articulos.stock', 'articulos.descripcion', 'articulos.condicion')
+                            ->orderBy('articulos.id', 'desc')->paginate(5);
+        } else {
+            $articulos = Articulo::join('categorias', 'articulos.idcategoria', '=', 'categorias.id')
+                            ->select('articulos.id', 'articulos.idcategoria', 'articulos.codigo', 'articulos.nombre', 'categorias.nombre as nombre_categoria', 'articulos.precio_venta', 'articulos.stock', 'articulos.descripcion', 'articulos.condicion')
+                            ->where('articulos' . $criterio, 'like', '%' . $buscar . '%')
+                            ->orderBy('articulos.id', 'desc')->paginate(5);
+        }
+
+
+
+        return [
+            'pagination' => [
+                'total'        => $articulos->total(),
+                'current_page' => $articulos->currentPage(),
+                'per_page'     => $articulos->perPage(),
+                'last_page'    => $articulos->lastPage(),
+                'from'         => $articulos->firstItem(),
+                'to'           => $articulos->lastItem(),
+            ],
+            'articulos' => $articulos
+        ];
+    }
+
+    public function store(Request $request) {
+        if (!$request->ajax()) return redirect('/');
+        $articulo = new Articulo();
+        $articulo->idcategoria = $request->idcategoria;
+        $articulo->codigo = $request->codigo;
+        $articulo->nombre = $request->nombre;
+        $articulo->precio_venta = $request->precio_venta;
+        $articulo->stock = $request->stock;
+        $articulo->descripcion = $request->descripcion;
+        $articulo->condicion = '1';
+        $articulo->save();
+    }
+
+    public function update(Request $request) {
+        if (!$request->ajax()) return redirect('/');
+        $articulo = Articulo::findOrFail($request->id);
+        $articulo->idcategoria = $request->idcategoria;
+        $articulo->codigo = $request->codigo;
+        $articulo->nombre = $request->nombre;
+        $articulo->precio_venta = $request->precio_venta;
+        $articulo->stock = $request->stock;
+        $articulo->descripcion = $request->descripcion;
+        $articulo->condicion = '1';
+        $articulo->save();
+    }
+
+    public function desactivar(Request $request) {
+        if (!$request->ajax()) return redirect('/');
+        $articulo = Articulo::findOrFail($request->id);
+        $articulo->condicion = '0';
+        $articulo->save();
+    }
+
+    public function activar(Request $request) {
+        if (!$request->ajax()) return redirect('/');
+        $articulo = Articulo::findOrFail($request->id);
+        $articulo->condicion = '1';
+        $articulo->save();
+    }
+
+}
