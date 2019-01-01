dfeb218d3b0c12e4eddf5664d20791d78326dcef
Francisco <Francisco@fmorales>
2019-01-01 13:25:00 -0300

    Relación Categoría Artículos.

--- app/Categoria.php
+++ app/Categoria.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     protected $fillable = ['nombre', 'descripcion', 'condicion'];
+    
+    public function articulos()
+    {
+        return $this->hasMany('App\Articulo');
+    }
 }
